027511483376ad05f03534e15bc0a43669ecd5e7
Roshan Kumar <roshankrsingh2000@gmail.com>
2021-04-18 21:37:42 +0530

    Fifth commit

---
+++ app/src/main/java/com/example/passcare/random_password.java
@@ -0,0 +1,14 @@
+package com.example.passcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class random_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_password);
+    }
+}
